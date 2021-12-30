67993b0541d6428b0daf1347c652b7233e4bee04
David Puerta Martos <dpuerta@usal.es>
2021-12-30 21:21:12 +0100

    current score saved in state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const Title = () => {
   return (
     <div className="appTitle">
@@ -9,12 +11,14 @@ const Title = () => {
   )
 }
 
-const CardDisplayer = () => {
+const CardDisplayer = ({currentScore, setScore}) => {
   const cards = [
-    {id: 1, title: 1}, {id: 2, title: 2}, {id: 3, title: 3}, 
-    {id: 4, title: 4}, {id: 5, title: 5}, {id: 6, title: 6}, 
-    {id: 7, title: 7}, {id: 8, title: 8}, {id: 9, title: 9}, 
-    {id: 10, title: 10}, {id: 11, title: 11}, {id: 12, title: 12}
+    {id: 1, title: 1, clicked: "no"}, {id: 2, title: 2, clicked: "no"}, 
+    {id: 3, title: 3, clicked: "no"}, {id: 4, title: 4, clicked: "no"}, 
+    {id: 5, title: 5, clicked: "no"}, {id: 6, title: 6, clicked: "no"}, 
+    {id: 7, title: 7, clicked: "no"}, {id: 8, title: 8, clicked: "no"}, 
+    {id: 9, title: 9, clicked: "no"}, {id: 10, title: 10, clicked: "no"}, 
+    {id: 11, title: 11, clicked: "no"}, {id: 12, title: 12, clicked: "no"}
   ]
   
   const shuffleArray = (array) => {
@@ -30,27 +34,35 @@ const CardDisplayer = () => {
   return (
     <div className="container">
       <div className="row row-cols-3">
-        {shuffleArray(cards).map(card=><Card key={card.title} cardToDisplay={card}/>)}
+        {shuffleArray(cards).map(card=><Card key={card.title} cardToDisplay={card} currentScore={currentScore} setScore={setScore}/>)}
       </div>
     </div>
   )
 }
 
-const Card = ({cardToDisplay}) => {
-  return <div className="col">{cardToDisplay.title}</div>
+const Card = ({cardToDisplay, currentScore, setScore}) => {
+  const cardFunction = (card) =>{
+    card.clicked="yes"
+    console.log(card)
+    setScore(currentScore+1)
+  }
+  return <div onClick={()=>cardFunction(cardToDisplay,setScore)} className="col">{cardToDisplay.title}</div>
 
 }
 
-const ScoreBoard = () => {
+const ScoreBoard = ({currentScore, maxScore}) => {
   return(
     <div>
-      <div>Current score: 0</div>
-      <div>Best score: 0</div>
+      <div>Current score: {currentScore}</div>
+      <div>Best score: {maxScore} </div>
     </div>
   )
 }
 
 const App = () => {
+  const [currentScore, setCurrentScore] = useState(0)
+  const [maxScore, setMaxScore] = useState(0)
+
   return (
     <div className="container mainContainer">
       <div className="row">
@@ -58,11 +70,11 @@ const App = () => {
           <Title />
         </div>
         <div className="col">
-          <ScoreBoard />
+          <ScoreBoard currentScore={currentScore} maxScore={maxScore}/>
         </div>
       </div>
       <div className="row">
-        <CardDisplayer />
+        <CardDisplayer currentScore={currentScore} setScore={setCurrentScore}/>
       </div>
       <div>Copyright</div>
     </div>
